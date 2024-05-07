cf6774811ea34a9ee53aab04f8ea366b6a495144
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-05-07 09:28:22 +0300

    AD

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,9 @@
-import { useState, useEffect} from 'react'
-import './App.module.css'
-import Description from '../Description/Description'
-import Options from '../Options/Options'
-import Feedback from '../Feedback/Feedback'
-import Notification from '../Notification/Notification'
 export default function App() {
-  const [values, setValues] = useState(() => {
-    const savedValues = window.localStorage.getItem("saved-values");
-if (savedValues !== null) {
-  return JSON.parse(savedValues)
-}
-return {
-  good: 0,
-  neutral: 0,
-  bad: 0
-};
-}  );
-  useEffect(() => {
-    window.localStorage.setItem("saved-values", JSON.stringify(values));
-  }, [values]); 
   
-  const updateFeedback = (feedbackType) => {
-    setValues(prevValues => ({
-       ...prevValues,
-        [feedbackType]: prevValues[feedbackType] + 1
-    }));
-};
-const resetFeedbacks = () => {
-  setValues({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
-};
-  const totalFeedback = values.good + values.neutral + values.bad;
-  const positiveFeedbacks = totalFeedback > 0? Math.round((values.good / totalFeedback) * 100):0; 
  
   return (
     <>
-     <Description />
-     <Options setValues={updateFeedback} resetFeedbacks={resetFeedbacks} totalFeedback={totalFeedback}/>
-     { totalFeedback > 0? <Feedback values={values} totalFeedback={totalFeedback} positiveFeedbacks={positiveFeedbacks}/>:<Notification/>}
-    </>
+    <h1>Hello</h1>
+     </>
   )
 }
--- src/components/Description/Description.jsx
+++
@@ -1,8 +0,0 @@
-import css from './Description.module.css'
-export default function Description() {
-return (<div className={css.div}>
-    <h1><span className={css.textColor}>Sip</span> Happens Café
-</h1>
-<p>Please leave your feedback <span className={css.textSpan}>about</span> our service <span className={css.textSpan}>by</span> selecting one <span className={css.textSpan}>of the</span> options <span className={css.textSpan}>below.</span></p>
-</div>)
-}
