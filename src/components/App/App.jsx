ec04f0a7bfc9a93a2a91b83ec664e894dcef6a52
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-05-08 14:58:47 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,23 @@ import css from './App.module.css'
 import ContactForm from '../ContactForm/ContactForm'
 import SearchBox from '../SearchBox/SearchBox'
 import ContactList from '../ContactList/ContactList'
-import initialData from '../../data/data.json'
-import { useState } from 'react';
+// import initialData from '../../data/data.json'
+import { useState, useEffect } from 'react';
 export default function App() {
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState(()=>{
+    const saveData = window.localStorage.getItem("saved-data");
+    if(saveData !== null) 
+      {return JSON.parse(saveData)}
+return [
+  { "id": "id-1", "name": "Rosie Simpson", "number": "459-12-56" },
+  { "id": "id-2", "name": "Hermione Kline", "number": "443-89-12" },
+  { "id": "id-3", "name": "Eden Clements", "number": "645-17-79" },
+  { "id": "id-4", "name": "Annie Copeland", "number": "227-91-26" }
+];
+  });
+  useEffect(() => {
+    window.localStorage.setItem("saved-data", JSON.stringify(data));
+  }, [data]);
   const [filter, setFilter] = useState('');
   const addData = (newData) => {
     setData((prevData) => {
